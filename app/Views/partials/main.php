98758d1f3a784b3d14587af0de059c9a2120b8ec
Agung Sugiarto <sugiartoagung92@gmail.com>
2020-05-12 22:32:23 +0700

    add copyright

--- app/Views/partials/main.php
+++ app/Views/partials/main.php
@@ -93,7 +93,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 Page rendered in {elapsed_time} seconds. Environment: <?= ENVIRONMENT ?>
             </div>
             <!-- Default to the left -->
-            <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights
+            <strong>Copyright &copy; <?= date('Y') ?> <a href="https:agungsugiarto.github.io">Agung Sugiarto</a>.</strong> All rights
             reserved.
         </footer>
     </div>
